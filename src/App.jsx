015e586b1ebf2ebed0e9dcbd86ee62a6ddf92212
dbszl3 <yoonji0811@naver.com>
2026-04-10 14:52:05 +0900

    레포지토리 연결

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+function App() {
+  const [text, setText] = useState("");
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
+  function handleClick() {
+    alert(text);
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={handleChange} />
+      <button onClick={handleClick}>확인</button>
+    </div>
+  );
+}
+
+export default App;
